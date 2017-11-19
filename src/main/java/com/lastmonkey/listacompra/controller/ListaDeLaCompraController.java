07852fdee040da3130e51b7a504a12d4d9695af3
macf3rn4nd0 <macf3rn4nd0@localhost>
2017-11-19 01:20:49 +0100

    MacbookPro con nueva display retina

--- src/main/java/com/lastmonkey/listacompra/controller/ListaDeLaCompraController.java
+++ src/main/java/com/lastmonkey/listacompra/controller/ListaDeLaCompraController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ListaDeLaCompraController {
 
 	
-	@GetMapping("/listadodoc")
+	@GetMapping("/listado")
 	public String helloWorld(){
 		
 		return "listado";
